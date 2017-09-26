0db0731fd38149753780d20f5add72782b2375e4
mich34 <biznesmichal9@gmail.com>
2017-09-26 10:05:38 +0200

    gfh

---
+++ zadania/js/DostępDoElementu.js
@@ -0,0 +1,65 @@
+//Metoda getElementById()
+
+var elPierwszy = document.getElementById('pierwszy');
+elPierwszy.className = 'niebieski';
+
+//Metoda getElementsByTagName()
+
+var elNaglowek = document.getElementsByTagName('h1');
+if (elNaglowek.length > 0){
+	console.log(elNaglowek);
+	elNaglowek[0].className = 'czerwony';
+}
+
+//Metoda getElementsByClassName()
+
+var czerwony = document.getElementsByClassName('czerwony');
+if(czerwony.length > 0){
+	console.log(czerwony.length);
+	console.log(czerwony);
+
+	//metoda wolniejsza
+	/*var ostatni = czerwony.item(czerwony.length - 1)*/
+
+	//metoda szybsza
+/*	var ostatni = czerwony[czerwony.length-1];*/
+/*	ostatni.className = 'niebieski';*/
+}
+
+//Metoda querySelector()
+
+//zwraca pierwsze dopasowanie
+
+var elTv = document.querySelector('li.grey');
+elTv.className = 'niebieski';
+
+//Metoda querySelectorAll()
+
+var elTvAll = document.querySelectorAll('li.grey');
+console.log(elTvAll.length);
+elTvAll[2].className = 'niebieski';
+
+//Metoda getElementsByName()
+
+/*var elPrzycisk = document.getElementById('Przycisk');
+//Ja zrobiłem sam
+elPrzycisk.onclick = function() {
+	var elAll = document.getElementsByName('TV');
+	for ( var i=0; i<elAll.length; i++){
+		elAll[i].className = 'niebieski';
+	}
+}*/
+
+//To samo w pętli forEach
+
+var elPrzycisk = document.getElementById('Przycisk');
+var elTvZmien = document.getElementsByName('TV');
+
+function zmien(element,index){
+	elTvZmien[index].className = 'niebieski';
+}
+elPrzycisk.onclick = function (){
+	elTvZmien.forEach(zmien);
+}
+
+//
